2ff8bfc418b819b84c7fb8906631c38c53a9ed6e
kb150802 <bansalkushagra150802@gmail.com>
2024-07-31 16:49:47 +0530

    contact image service added

--- src/main/java/com/app/ContactManager/service/ContactService.java
+++ src/main/java/com/app/ContactManager/service/ContactService.java
@@ -4,12 +4,13 @@ import com.app.ContactManager.model.Contact;
 import com.app.ContactManager.model.User;
 import com.app.ContactManager.repository.ContactRepository;
 import com.app.ContactManager.repository.UserRepository;
-import jakarta.persistence.Access;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @Service
@@ -18,6 +19,10 @@ public class ContactService {
     ContactRepository contactRepository;
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    FileStorageService fileStorageService;
+
     public ResponseEntity<?> deleteContact(String username, Integer cId) {
         try {
             Contact contact = contactRepository.getContactById(cId);
@@ -29,6 +34,7 @@ public class ContactService {
             contacts.remove(contact);
             userRepository.save(user);
             contactRepository.save(contact);
+            contactRepository.delete(contact);
             return new ResponseEntity<>("Contact deleted successfully", HttpStatus.OK);
         }catch(Exception exception) {
             return new ResponseEntity<>("Failed to delete contact", HttpStatus.INTERNAL_SERVER_ERROR);
@@ -57,5 +63,46 @@ public class ContactService {
             return new ResponseEntity<>("Failed to update contact", HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
+    }
+
+    public ResponseEntity<?> addImage(String username,Integer cId, MultipartFile file) {
+        try{
+            User user = userRepository.findUserByUsername(username);
+            if(user == null) {
+                return new ResponseEntity<>("User not found", HttpStatus.BAD_REQUEST);
+            }
+            Contact contact = contactRepository.getContactById(cId);
+            if(contact == null) {
+                return new ResponseEntity<>("Contact not found", HttpStatus.BAD_REQUEST);
+            }
+
+            if(!contact.getUser().getEmail().equals(username)) {
+                return new ResponseEntity<>("Can not upload image", HttpStatus.FORBIDDEN);
+            }
+            String imageUrl = fileStorageService.storeFile(file);
+            contact.setImage(file.getOriginalFilename());
+            contactRepository.save(contact);
+            return new ResponseEntity<>("Image uploaded successfully", HttpStatus.OK);
+        }catch (Exception exception) {
+            return new ResponseEntity<>("Failed to upload Image", HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+    }
+
+    public ResponseEntity<?> getImage(String username, Integer cId) {
+        try{
+            Contact contact = contactRepository.getContactById(cId);
+            if(contact == null) {
+                return new ResponseEntity<>("Contact not found",HttpStatus.BAD_REQUEST);
+            }
+            if(!contact.getUser().getEmail().equals(username)) {
+                return new ResponseEntity<>("Can not get image", HttpStatus.FORBIDDEN);
+            }
+
+            Resource resource = fileStorageService.getFile(contact.getImage());
+            return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(resource.getContentAsByteArray());
+        }catch (Exception exception) {
+            return new ResponseEntity<>("Failed to fetch image", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
